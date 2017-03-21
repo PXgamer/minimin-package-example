7efd998e7ecf71250576b4a9ae696fd884e67cd0
PXgamer <owzie123@gmail.com>
2017-03-21 21:55:39 +0000

    Corrected info

--- src/Plugin.php
+++ src/Plugin.php
@@ -15,9 +15,9 @@ class Plugin
         
         $object = (object)[
             'app_namespace' => '\\MiniminPackage\\ExamplePackage', // The namespace of the plugin
-            'name' => 'Cron Tasks', // The name of the plugin, may contain spaces
-            'link' => 'cron-tasks', // The link that will be used (e.g. `cron-tasks` will be `/cron-tasks`)
-            'description' => 'A cron task manager for servers.' // The description of the plugin
+            'name' => 'Example Package', // The name of the plugin, may contain spaces
+            'link' => 'example', // The link that will be used (e.g. `cron-tasks` will be `/cron-tasks`)
+            'description' => 'An example package for Minimin.' // The description of the plugin
         ];
         
         return $object;
