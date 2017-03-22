a23a6106cb9974da485ce2d37a5afb3d9cc65e72
PXgamer <owzie123@gmail.com>
2017-03-22 12:00:34 +0000

    Added new format for package example

--- src/App.php
+++ src/App.php
@@ -2,11 +2,21 @@
 
 namespace MiniminPackage\ExamplePackage;
 
+use pxgamer\Minimin\Smarter;
+
 class App
 {
     // When initialised by the Minimin class, it will pass the $route to the plugin App constructor
-    public function __construct($route) {
+    public function __construct($route)
+    {
         // ... Run plugin commands
-		var_dump($route);
+        $Smarter = Smarter::get();
+        $Smarter->addTemplateDir(__DIR__ . '/Templates/');
+        $Smarter->display(
+            'ExamplePackage\index.tpl',
+            [
+                'route' => $route
+            ]
+        );
     }
 }
---
+++ src/Templates/ExamplePackage/index.tpl
@@ -0,0 +1,4 @@
+<div class="container">
+    <h1>Example Plugin</h1>
+    {$route|var_dump}
+</div>
